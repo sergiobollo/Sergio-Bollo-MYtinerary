3910cabe0f1150c6d35673dff5b4b87d0bebc4db
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-01-29 01:31:23 -0300

    Update cities.js

--- server/routes/cities.js
+++ server/routes/cities.js
@@ -15,65 +15,64 @@ router.get('/all',
             .catch(err => console.log(err));
     });
 
-//router.post('/', (req, res) => {
-//const newCity = new cityModel({
-//name: req.body.name,
-//country: req.body.country
-//})
-//newCity.save()
-//.then(city => {
-//res.send(city)
-//})
-//.catch(err => {
-//res.status(500).send("Server error")})
-//});
-
-
-router.post('/', (req, res, next) => {
- 
-    const inputCity = req.body.name; //Extract title from input form
-    cityModel.findOne({name:inputCity}, function(err, city){
-        if(err) console.log(err);
-        if (city){
-            console.log("This has already been saved");
-        } else {
- 
-            const newCity = new cityModel({
-            name: req.body.name,
-            country: req.body.country
-            })
-            newCity.save()
-            .then(city => {
+router.post('/', (req, res) => {
+    const newCity = new cityModel({
+        name: req.body.name,
+        country: req.body.country
+    })
+    newCity.save()
+        .then(city => {
             res.send(city)
-            })
-            .catch(err => {
-            res.status(500).send("Server error")});
-                
-                console.log("New example created");
-                res.redirect(`/`);
-            });
-        }
-    });
+        })
+        .catch(err => {
+            res.status(500).send("Server error")
+        })
 });
 
 module.exports = router
 
+/*
+router.post('/', (req, res, next) => { 
+    const inputCity = req.body.name; //Extract title from input form
+    cityModel.findOne({ name: inputCity }, function(err, city) {
+        if (err) console.log(err);
+        if (city) {
+            console.log("This has already been saved");
+        } else { 
+            const newCity = new cityModel({
+                name: req.body.name,
+                country: req.body.country
+            })
+            newCity.save()
+                .then(city => {
+                    res.send(city)
+                })
+                .catch(err => {
+                    res.status(500).send("Server error")
+                });
+            console.log("New example created");
+            res.redirect(`/`);
+        };
+    })
+});*/
+
+
 // router.post('/example', (req, res, next) => {
  
-   // var query = req.body.title; //Extract title from input form
-   // Example.findOne({title:query}, function(err, example){
-     //   if(err) console.log(err);
-      //  if ( example){
-     //       console.log("This has already been saved");
-     //   } else {
+// var query = req.body.title; //Extract title from input form
+// Example.findOne({title:query}, function(err, example){
+//   if(err) console.log(err);
+//  if ( example){
+//       console.log("This has already been saved");
+//   } else {
  
-     //      var example = new Example(req.body);
-       //     example.save(function(err, example) {
-         //       if(err) console.log(err);
-           //     console.log("New example created");
-             //   res.redirect(`/`);
-            });
-     //   }
-   // });
+//      var example = new Example(req.body);
+//     example.save(function(err, example) {
+//       if(err) console.log(err);
+//     console.log("New example created");
+//   res.redirect(`/`);
+//   });
+//   }
+// });
 // });
 // https://medium.com/@thiscodeworks.com/how-to-save-non-duplicate-unique-documents-in-mongodb-via-mongoose-node-js-4a26b0336ab3
