f2697bd3f632e0a1baedd9033c51ebe9a341ae43
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-04 12:43:20 -0300

    Update rootReducer.js

--- client/src/store/reducers/rootReducer.js
+++ client/src/store/reducers/rootReducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
 import FilterCitiesReducer from "./citiesReducer";
 import FilterFormReducer from "./FilterFormReducer";
+import ItinerariesReducer from "itineraryReducer";
 
-const rootReducer = combineReducers({ cities: FilterCitiesReducer, cityFilter: FilterFormReducer });
+const rootReducer = combineReducers({ cities: FilterCitiesReducer, cityFilter: FilterFormReducer, itineraries: ItinerariesReducer });
 
 export default rootReducer;
