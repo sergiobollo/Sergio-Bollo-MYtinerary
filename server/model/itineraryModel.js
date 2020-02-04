ccacbc8ec63d0e54512dbfe3fca701a3a7581a9b
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-04 10:53:15 -0300

    Create itineraryModel.js

---
+++ server/model/itineraryModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const itinerarySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    rating: {
+        type: String,
+        required: true
+    },
+    profilePicture: {
+        type: String
+    },
+    duration: {
+      type: String
+    },
+    price: {
+        type: String
+    },
+    hashtags: {
+      type: String
+    }
+})
+
+module.exports = mongoose.model('itinerary', itinerarySchema)
