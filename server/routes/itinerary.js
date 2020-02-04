f6268a80219182ffc9417400282ddb310fba6103
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-04 10:06:03 -0300

    Create itinerary.js

---
+++ server/routes/itinerary.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const itineraryModel = require('../model/itineraryModel');
+
+//this is code that you have already implemented in your project
+router.get('/all',
+(req, res) => {
+itineraryModel.find({})
+.then(files => {
+res.send(files)
+})
+.catch(err => console.log(err));
+});
+
+//this is how you implement a city route by specific city
+router.get('/:name',
+(req, res) => {
+let cityRequested = req.params.name;
+cityModel.findOne({ name: cityRequested })
+.then(city => {
+res.send(city)
+})
+.catch(err => console.log(err));
+});
