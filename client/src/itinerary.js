f252383ed72ed40673903ba19786bf23d1eeec4c
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-04 14:17:15 -0300

    Update itinerary.js

--- client/src/itinerary.js
+++ client/src/itinerary.js
@@ -6,12 +6,37 @@ class Itinerary extends React.Component {
 
  fetchItineraries = () => {
         // this.setState({...this.state, isFetching: true })
-        fetch("http://localhost:5000/itineraries/:name")
+        fetch("http://localhost:5000/itineraries/all")
             .then(response => response.json())
-            .then(result => this.props.listarCiudades(result))
+            .then(result => this.props.listarItinerarios(result))
             .catch(e => console.log(e))
+    }
+ 
+     componentDidMount() {
+        this.fetchCities()
+        console.log(this)
+    }
+
+
+}
+
+const mapDispactchToProps = (dispatch) => {
+    return {
+        listarItinerarios: (result) => dispatch({ type: LISTAR_ITINERARIOS, payload: result }),
+        filtrarItinerarios: (cityName) => dispatch({ type: FILTER_ITINERARIES, payload: cityName })
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        itineraries: state.itineraries.itineraries,
+        filteredItineraries: state.itineraries.filteredItineraries,
+        isFetching: state.isFetching
     }
 
 }
+
+
+
 
 export default connect(mapStateToProps, mapDispactchToProps)(FilterItineraries)
