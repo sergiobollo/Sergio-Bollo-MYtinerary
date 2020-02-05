595639a990c0c59a29518a864445fb882edce701
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-05 17:47:08 -0300

    Update rootReducer.js

--- client/src/store/reducers/rootReducer.js
+++ client/src/store/reducers/rootReducer.js
@@ -2,7 +2,8 @@ import { combineReducers } from "redux";
 import FilterCitiesReducer from "./citiesReducer";
 import FilterFormReducer from "./FilterFormReducer";
 import ItinerariesReducer from "./itineraryReducer";
+import usersReducer from "./usersReducer";
 
-const rootReducer = combineReducers({ cities: FilterCitiesReducer, cityFilter: FilterFormReducer, itineraries: ItinerariesReducer });
+const rootReducer = combineReducers({ cities: FilterCitiesReducer, cityFilter: FilterFormReducer, itineraries: ItinerariesReducer, user: usersReducer });
 
 export default rootReducer;
