d98111654e1fa616407821eeb3f92629c75c7326
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-05 14:28:52 -0300

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,8 +3,20 @@ const express = require('express');
 const router = express.Router();
 const userModel = require('../model/userModel')
 
-router.put()
-
+router.post('/', (req, res) => {
+    const newUser = new userModel({
+        email: req.body.email,
+        password: req.body.password,
+        picture: req.body.pictute
+    })
+    newUser.save()
+        .then(user => {
+            res.send(user)
+        })
+        .catch(err => {
+            res.status(500).send("Server error")
+        })
+});
 
 
 module.exports = router
