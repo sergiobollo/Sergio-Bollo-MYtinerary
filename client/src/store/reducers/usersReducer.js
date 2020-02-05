fe8e3edc2559f57cc9dfabe23be598a835a2c4cd
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-05 17:39:24 -0300

    Create usersReducer.js

---
+++ client/src/store/reducers/usersReducer.js
@@ -0,0 +1,17 @@
+const initilState = { user: {}, login:{} };
+
+const usersReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'POST_USER': return {
+            ...state,
+            user: action.payload
+        }
+        case 'LOGIN_USER': return {
+            ...state,
+            login: action.payload
+        }
+        default: return state;
+    }
+};
+
+export default usersReducer
