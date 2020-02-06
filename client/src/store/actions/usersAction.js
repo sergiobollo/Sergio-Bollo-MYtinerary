b76ebf674e79c19cf2357e711d0d9aa5ebcc78ab
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-06 11:50:36 -0300

    Update usersAction.js

--- client/src/store/actions/usersAction.js
+++ client/src/store/actions/usersAction.js
@@ -1,3 +1,6 @@
 export const POST_EMAIL = 'POST_EMAIL';
 export const POST_PASSWORD = 'POST_PASSWORD';
 export const POST_PICTURE = 'POST_PICTURE';
+export const LOG_EMAIL = 'LOG_EMAIL';
+export const LOG_PASSWORD = 'LOG_EPASSWORD';
+export const LOGIN = 'LOGIN';
