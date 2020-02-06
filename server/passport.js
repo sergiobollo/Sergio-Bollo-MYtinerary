4cc3efe8580990407bf57a0700b47690a950e1dd
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-06 16:31:33 -0300

    Update passport.js

--- server/passport.js
+++ server/passport.js
@@ -3,3 +3,20 @@ const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("user");
 const key = require("./key");
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey= key.secretOrKey;
+
+module.exports = passport.use(
+new JwtStrategy(opts, (jwt_payload, done) => {
+User.findById(jwt_payload.id)
+.then(user => {
+if (user) {
+return done(null, user);
+}
+return done(null, false);
+})
+.catch(err => console.log(err));
+})
+);
