9383fbb67127ac0c8d3d4fedffbf4c21dee3ff09
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-06 11:24:04 -0300

    Update Login.js

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,7 +1,78 @@
 import React, { Component } from 'react';
 
-export default class Login extends Component {
+class Login extends React.Component {
+    
+    
+      handleSubmit = async () => {
+        await this.props.login(this.props.param);
+        
+        alert("Login", this.props.logged);
+        this.onLoginSuccess(this.props.logged.token)
+
+    }
+    
+    
+    handleEmailChange(event) {
+        this.props.ingresarEmail(event.target.value);
+    }
+
+    handlePasswordChange(event) {
+        this.props.ingresarPassword(event.target.value);
+    }
+    
     render() {
-        return <h1 > Login < /h1>;
+        return(
+        <React.Fragment>
+         <h1 > Login < /h1>
+                
+            <
+            form onSubmit = { this.handleSubmit.bind(this) } >
+            <
+            label >
+            Email:
+            <
+            input placeholder = "Email"
+            type = "email"
+            value = { this.props.email }
+            onChange = { this.handleEmailChange.bind(this) }
+            /> < /
+            label > <
+            label >
+            Password:
+            <
+            input placeholder = "Password"
+            type = "password"
+            value = { this.props.password }
+            onChange = { this.handlePasswordChange.bind(this) }
+            /> < /
+            label >
+
+            <
+            input type = "submit"
+            value = "Submit" / >
+            <
+            /form>
+            
+         </React.Fragment>
+        );
     }
 }
+
+const mapDispactchToProps = (dispatch) => {
+    return {
+        ingresarEmail: (value) => dispatch({ type: LOG_EMAIL, payload: value }),
+        ingresarPassword: (value) => dispatch({ type: LOG_PASSWORD, payload: value }),
+        login: (value) => dispatch({ type: LOGIN, payload: value }),
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        params: state.user,
+        email: state.user.email,
+        password: state.user.password,
+        logged: state.user.logged
+    };
+}
+
+export default connect(mapStateToProps, mapDispactchToProps)(Login)
