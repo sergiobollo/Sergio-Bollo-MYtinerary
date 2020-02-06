efc9c7e08bacdfccd19014f45255499b3edee7fe
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-06 16:36:47 -0300

    Update authentication.js

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -3,7 +3,20 @@ var router = express.Router();
 const key= require('.../keys');
 const jwt= require('jsonwebtoken');
 const userModel = require('../model/userModel');
+const passport= require('passport');
 
+router.get(
+"/",
+passport.authenticate("jwt", { session: false }),
+(req, res) => {
+userModel
+.findOne({ _id: req.user.id })
+.then(user => {
+res.json(user);
+})
+.catch(err => res.status(404).json({ error: "User does not
+exist!" }));
+}
 
 router.post('/login', function (req, res) {
     userModel.findOne({ email: req.body.email })
