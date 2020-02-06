b58eb46b10fbf26f5861a64eb255e34c1a57530e
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-06 01:10:45 -0300

    sprint3

--- client/src/itinerary.js
+++ client/src/itinerary.js
@@ -5,56 +5,57 @@ import { FILTER_ITINERARIES, LISTAR_ITINERARIOS } from "./store/actions/itinerar
 
 class Itinerary extends React.Component {
 
-    fetchItineraries = () => {
-        // this.setState({...this.state, isFetching: true })
-        fetch(this.props.url)
-            .then(response => response.json())
-            .then(result => this.props.listarItinerarios(result))
-            .catch(e => console.log(e))
-    }
+        fetchItineraries = () => {
+            // this.setState({...this.state, isFetching: true })
+            fetch(this.props.url)
+                .then(response => response.json())
+                .then(result => this.props.listarItinerarios(result))
+                .catch(e => console.log(e))
+        }
 
-    componentDidMount() {
-        this.fetchItineraries()
-        console.log(this.props.url)
-    }
+        componentDidMount() {
+            this.fetchItineraries()
+            console.log(this.props.url)
+        }
 
 
-    render() {
-        return ( <
-            React.Fragment >
-            <
-            h1 > { this.props.city } < /h1> <
-            ul > {
-                this.props.itineraries.map((itinerary) =>
-                    <
-                    li key = { itinerary.title }
-                    value = { itinerary.title } > { itinerary.title }, { itinerary.username }, { itinerary.rating }, { itinerary.profilePicture }, { itinerary.duration }, { itinerary.price }, { itinerary.hashtags }, { itinerary.activities } < /li >
+        render() {
+            return ( <
+                React.Fragment >
+                <
+                h1 > { this.props.city } < /h1> <
+                ol > {
+                    this.props.itineraries.map((itinerary) =>
+                        <
+                        li key = { itinerary.title }
+                        value = { itinerary.title } > { itinerary.title }, { itinerary.username }, { itinerary.rating }, { itinerary.profilePicture }, { itinerary.duration }, { itinerary.price }, { itinerary.hashtags }, {
+                            itinerary.activities.map((activity) => < ul > < li key = { activity } > { activity } < /li > < /ul > )
+                        } < /li > )
+                    } < /ol> < /
+                    React.Fragment >
                 )
-            } < /ul> < /
-            React.Fragment >
-        )
-    }
+            }
 
-}
+        }
 
-const mapDispactchToProps = (dispatch) => {
-    return {
-        listarItinerarios: (result) => dispatch({ type: LISTAR_ITINERARIOS, payload: result }),
-        filtrarItinerarios: (cityName) => dispatch({ type: FILTER_ITINERARIES, payload: cityName })
-    }
-}
+        const mapDispactchToProps = (dispatch) => {
+            return {
+                listarItinerarios: (result) => dispatch({ type: LISTAR_ITINERARIOS, payload: result }),
+                filtrarItinerarios: (cityName) => dispatch({ type: FILTER_ITINERARIES, payload: cityName })
+            }
+        }
 
-const mapStateToProps = (state) => {
-    return {
-        itineraries: state.itineraries.itineraries,
-        url: state.itineraries.url,
-        isFetching: state.isFetching,
-        city: state.itineraries.city
-    }
+        const mapStateToProps = (state) => {
+            return {
+                itineraries: state.itineraries.itineraries,
+                url: state.itineraries.url,
+                isFetching: state.isFetching,
+                city: state.itineraries.city
+            }
 
-}
+        }
 
 
 
 
-export default connect(mapStateToProps, mapDispactchToProps)(Itinerary)
+        export default connect(mapStateToProps, mapDispactchToProps)(Itinerary)
