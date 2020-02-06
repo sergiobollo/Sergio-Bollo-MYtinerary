b75525a334e7ea464e2a2bb58fc079799d24eb7b
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-06 16:06:45 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,11 @@ app.use('/cities', require('./routes/cities'));
 app.use('/itineraries', require('./routes/itineraries'));
 app.use('/users', require('./routes/users'));
 
+//passport middleware
+app.use(passport.initialize());
+//passport configuration
+require("./passport");
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("Server is running on " + port + "port");
