fed6fa1cc1f733d222aa04eac47fa77a6b66100b
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-06 13:07:58 -0300

    Update keys.js

--- server/keys.js
+++ server/keys.js
@@ -2,3 +2,7 @@ module.exports = {
 mongoURI:
 'mongodb+srv://sabollo:aled1982@mytinerarycluster-bvb7z.mongodb.net/MYtinerary?retryWrites=true&w=majority',
 };
+
+module.export={
+    secretOrKey: "secret"
+};
