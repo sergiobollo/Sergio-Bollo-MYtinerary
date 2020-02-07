475e10a549abc04d08ab157a52b232262779a5ce
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-07 16:38:04 -0300

    Create home.js

---
+++ client/src/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function HomeLink(props) {
+    return <
+            Link to = "/" > < img src = "./MYtineraryLogo.png"
+    alt = "Logo"
+    className = "App-home mt-4" /
+        >
+         Home < /Link> < /
+}
+
+export default HomeLink;
