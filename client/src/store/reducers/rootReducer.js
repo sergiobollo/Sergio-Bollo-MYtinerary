fcca8f1fa87eeb24f2b28a8f3c9b76b64f7dbf97
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-02-07 09:45:41 -0300

    Update rootReducer.js

--- client/src/store/reducers/rootReducer.js
+++ client/src/store/reducers/rootReducer.js
@@ -3,7 +3,8 @@ import FilterCitiesReducer from "./citiesReducer";
 import FilterFormReducer from "./FilterFormReducer";
 import ItinerariesReducer from "./itineraryReducer";
 import usersReducer from "./usersReducer";
+import registerReducer from "./registerReducer";
 
-const rootReducer = combineReducers({ cities: FilterCitiesReducer, cityFilter: FilterFormReducer, itineraries: ItinerariesReducer, user: usersReducer });
+const rootReducer = combineReducers({ cities: FilterCitiesReducer, cityFilter: FilterFormReducer, itineraries: ItinerariesReducer, user: usersReducer, token: registerReducer });
 
 export default rootReducer;
